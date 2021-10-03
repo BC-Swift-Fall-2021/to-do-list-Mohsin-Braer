f76dd62eab438c30f297d6c81006491008dc561b
Mohsin Braer <hbraer@gmail.com>
2021-10-02 23:26:39 -0400

    5.11

--- ToDoList/AppDelegate.swift
+++ ToDoList/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        UNUserNotificationCenter.current() = self;
+        UNUserNotificationCenter.current().self;
         
         return true
     }
